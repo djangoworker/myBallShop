2e8ebec1cd3a96d8c0b996235a7c6cf6591f7807
djangoworker <djangoworker@163.com>
2018-03-04 16:05:16 +0800

    写了HelloDjango程序

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello_view(request):
+	from django.http import HttpResponse
+	return HttpResponse("Hello,Django!")
+	
+	
+	
+	
+	'''
+	dict = {}
+	dict['str'] = "Hello,Django！"
+	return JsonResponse(dict)
+	'''
